7633ce02d3588dee6caa8db63fb64d41511103c1
Thomas William Page <tom.w.page@gmail.com>
2017-04-12 20:47:38 +0200

    some db changes

--- remindatext/app/controllers/todos_controller.rb
+++ remindatext/app/controllers/todos_controller.rb
@@ -1,8 +1,21 @@
 class TodosController < ApplicationController
 
   def index
-    @todos = Todo.all
-    @hi = "hi"
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    if @todo.save
+      redirect_to '/todos'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:date, :time, :activity)
   end
 
 
